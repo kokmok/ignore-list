84b3dce6fdc2d29c91c20c672652e10b619b2fa8
Jona <jona@solid-kiss.be>
2017-09-06 23:56:30 +0200

    try to append custom fields to serializer

--- plugin.rb
+++ plugin.rb
@@ -7,12 +7,12 @@ DiscoursePluginRegistry.serialized_current_user_fields << "ignored-users" #to ch
 after_initialize do
   User.register_custom_field_type('ignored-users', :text)
 
-  add_to_serializer(:user, :custom_fields, false) {
-    if object.custom_fields == nil then
-      object.custom_fields['ignored-users']
-    else
-      object.custom_fields
-    end
-  }
+  # add_to_serializer(:user, :custom_fields, false) {
+  #   if object.custom_fields == nil then
+  #     object.custom_fields['ignored-users']
+  #   else
+  #     object.custom_fields
+  #   end
+  # }
 
 end
