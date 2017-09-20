23ed0ac7020b69884309c158ea4a1f3c66c6b035
Jona <jona@solid-kiss.be>
2017-09-20 22:55:37 +0200

    cleaned

--- assets/javascripts/discourse/initializers/alert.js.es6
+++ assets/javascripts/discourse/initializers/alert.js.es6
@@ -1,8 +1,33 @@
-export default {
-    name: 'initlog',
-    initialize(container) {
-        const store = container.lookup('store:main');
+import { withPluginApi } from 'discourse/lib/plugin-api';
+import RawHtml from 'discourse/widgets/raw-html';
 
-        console.log('ignore-list plugin link is active');
+function hideContent(api, siteSettings) {
+  // api.includePostAttributes('custom_fields');
+  // api.includeUserAttributes('ignored_users_key');
+
+  api.decorateWidget('post-contents:after-cooked', dec => {
+
+    const attrs = dec.attrs;
+    console.log('attrs');
+    console.log(attrs);
+    const currentUser = api.getCurrentUser();
+    console.log(currentUser);
+    if (currentUser) {
+      const ignoredUsers = currentUser.custom_fields[currentUser.ignored_users_key].split(',');
+      if (enabled) {
+        return [dec.h('hr'), dec.h('div', new RawHtml({html: `<div class='user-signature'>LOL</div>`}))];
+
+      }
     }
+  });
+}
+
+export default {
+  name: 'ignore-list',
+  initialize(container) {
+    const siteSettings = container.lookup('site-settings:main');
+    // if (siteSettings.signatures_enabled) {
+      withPluginApi('0.1', api => hideContent(api, siteSettings));
+    // }
+  }
 };
--- plugin.rb
+++ plugin.rb
@@ -1,18 +1,35 @@
 # name: ignore-list
-# about: this plugin let you ignore some user posts
+# about: this plugin let you ignore some user's posts
 # version: 0.0.1
 # authors: Jonathan Cambier
 
-DiscoursePluginRegistry.serialized_current_user_fields << "ignored-users" #to change for a dynamic use later
+DiscoursePluginRegistry.serialized_current_user_fields << "ignored_users" #to change for a dynamic use later
+PLUGIN_NAME = "ignored_users".freeze
 after_initialize do
-  User.register_custom_field_type('ignored-users', :text)
+  User.register_custom_field_type('ignored_users', :text)
 
-  # add_to_serializer(:user, :custom_fields, false) {
-  #   if object.custom_fields == nil then
-  #     object.custom_fields['ignored-users']
-  #   else
-  #     object.custom_fields
-  #   end
-  # }
+  Rails.logger.error("IgnoredUsers v: 0.1");
+
+  module ::IgnoreList
+    class Engine < ::Rails::Engine
+      engine_name PLUGIN_NAME
+      isolate_namespace IgnoreList
+    end
+  end
+
+  add_to_serializer(:user, :custom_fields, false) {
+    if object.custom_fields == nil then
+      {}
+    else
+      object.custom_fields
+    end
+  }
+  add_to_serializer(:user, :ignored_users_key, false) {
+    if object.custom_fields == nil then
+      {}
+    else
+      "user_field_#{(UserField.find_by name: 'ignored_users').id}"
+    end
+  }
 
 end
